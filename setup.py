3c1086f7c9d845927396640a764f4fc8dd4e64cc
henrique <iklobato1@gmail.com>
2024-05-28 15:54:04 -0300

    build(setup): add description

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import codecs
+
 from setuptools import setup, find_packages
+
+with codecs.open('README.md', 'r', 'utf-8') as file:
+    long_description = file.read()
 
 setup(
     name='LightApi',
     version='0.1.0',
     description='A lightweight framework for building API endpoints using Python\'s native libraries.',
-    author='Your Name',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['api', 'rest', 'restful', 'endpoint', 'lightweight', 'framework'],
+    author='iklobato',
     author_email='iklobato1@gmail.com',
     url='https://github.com/henriqueblobato/LightApi',
     packages=find_packages(),
