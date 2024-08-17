ae3a523e2698e0618618aed14d7848612b842688
henrique <iklobato1@gmail.com>
2024-08-17 15:59:02 -0300

    fix(example): use one script example

--- examples/person_endpoint.py
+++ examples/person_endpoint.py
@@ -1,5 +1,41 @@
 from lightapi import LightApi
-from models import Person, Company
+from database import Base
+
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class Person(Base):
+    __tablename__ = "person"
+
+    pk = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    email_verified = Column(Boolean, default=False)
+
+    def as_dict(self):
+        return {
+            "pk": self.pk,
+            "name": self.name,
+            "email": self.email,
+            "email_verified": self.email_verified,
+        }
+
+
+class Company(Base):
+    __tablename__ = "company"
+
+    pk = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    website = Column(String)
+
+    def as_dict(self):
+        return {
+            "pk": self.pk,
+            "name": self.name,
+            "email": self.email,
+            "website": self.website,
+        }
 
 
 if __name__ == '__main__':
