5d4158bfdff5310a2933d967e63ab51979f0cb63
iklobato <iklobato1@gmail.com>
2024-09-12 22:19:31 -0300

    Enhance REST API with Advanced Authentication and Endpoint Configurat… (#8)
    * Enhance REST API with Advanced Authentication and Endpoint Configuration (#6)
    
    * Added MkDocs Material documentation and development dependencies
    
    - Created `requirements-dev.txt` for development dependencies, including libraries for modifying the project.
    - Added documentation pages: Home, Getting Started, Testing Handlers, License, and Reference.
    - Included some trial docstrings in the `lightapi.lightapi` module as examples, but they need further review.
    - Added email links to the repository creator in the documentation.
    - The documentation is maintainable through the `mkdocs.yml` file.
    - Set up the documentation to be compatible with GitHub Pages, including comment sections on each documentation page.
    
    * Create pages-publish.yml
    
    Added GitHub Actions workflow for automatic documentation publishing
    
    - Created a new GitHub Actions workflow file to automate publishing of the documentation to GitHub Pages.
    - The documentation is automatically deployed on every commit to the `main` branch.
    - Ensured compatibility with GitHub Pages for seamless updates after each commit.
    
    Feel free to change it.
    
    * Fixed site_url in MkDocs configuration
    
    - Corrected the `site_url` in the MkDocs configuration to point to the correct GitHub Pages URL: `user.github.io/repo_name`.
    
    * Fixed site_url in MkDocs configuration
    
    - Corrected the `site_url` in the MkDocs configuration to point to the correct GitHub Pages URL: `user.github.io/repo_name`.
    
    * Fixed repository URL for git-committers plugin
    
    - Corrected the repository URL in the MkDocs configuration for the `git-committers` plugin to ensure accurate contributor information.
    
    * Fixed links on the homepage
    
    - Corrected broken or incorrect links on the homepage to ensure proper navigation.
    
    * fix: updated documentation
    
    - Changed the documentation language.
    - Added 'docs' extra_require section in setup.py for documentation-related dependencies.
    - Updated installation instructions in installation.md to use `pip install light-api`.
    
    * Restored pip command to LightApi
    
    * Enhance REST API with Advanced Authentication and Endpoint Configuration
    
    - Added `AbstractAuthentication` class for flexible authentication mechanisms.
    - Implemented `DefaultJWTAuthentication` with dynamic SECRET_KEY generation.
    - Updated `RestEndpoint` class to support custom authentication and configuration options.
    - Added detailed docstrings and usage examples for `RestEndpoint`.
    - Integrated advanced example of `RestEndpoint` configuration including custom authentication, HTTP method handling, and route exclusion.
    
    These changes provide a more secure and flexible API framework, supporting advanced authentication and endpoint customization.
    
    * Repo link updated
    
    * Update pages-publish.yml
    
    * Refactor RestEndpoint to use HttpEnum for HTTP method handling
    
    - Introduced `HttpEnum` to enumerate HTTP methods.
    - Refactored `_create_routes` method in `RestEndpoint` to use `HttpEnum` for defining routes.
    - Updated `RestEndpoint` class to dynamically generate route handlers based on `HttpEnum`.
    - Simplified route creation logic by iterating over `HttpEnum` values.
    - Ensured consistency and maintainability by removing hardcoded HTTP methods.
    
    ---------
    
    Co-authored-by: digiagiulio <digiagiulio@gmail.com>
    
    * docs
    
    ---------
    
    Co-authored-by: digiagiulio <digiagiulio@gmail.com>

--- lightapi/lightapi.py
+++ lightapi/lightapi.py
@@ -1,3 +1,5 @@
+from typing import Callable, Dict, Type
+
 from sqlalchemy.exc import SQLAlchemyError
 
 from lightapi.database import engine, Base
@@ -6,6 +8,7 @@ from lightapi.handlers import create_handler
 import logging
 from aiohttp import web
 
+from lightapi.rest import RestEndpoint
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,63 +17,42 @@ logging.basicConfig(
 )
 
 
-class CustomApplication(web.Application):
-    """
-    Custom web application class that extends aiohttp.web.Application.
-
-    Methods:
-        _handle: Handles incoming requests with additional logging.
-    """
-    
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the CustomApplication instance.
-        """
-
-        super().__init__(*args, **kwargs)
-
-    async def _handle(self, *args, **kwargs):
-        """
-        Handles incoming requests with logging.
-
-        Returns:
-            aiohttp.web.Response: The response from the handler.
-        """
-
-        logging.info(f"Handling request: {args}, {kwargs}")
-        return super()._handle(*args, **kwargs)
-
-
 class LightApi:
     """
-    LightAPI framework class for managing routes and initializing the web application.
+    The main application class for managing routes and running the server.
 
-    This class is responsible for initializing the web application, registering
-    models, and creating database tables using SQLAlchemy.
+    This class registers routes for both SQLAlchemy models and custom `RestEndpoint` subclasses. It initializes
+    the application, creates database tables, and provides methods to register routes and start the server.
 
     Attributes:
-        app (aiohttp.web.Application): The aiohttp application instance.
-        routes (list): List of registered routes for the application.
+        app (web.Application): The aiohttp application instance.
+        routes (List[web.RouteDef]): A list of route definitions to be added to the application.
 
     Methods:
-        register: Registers API routes for SQLAlchemy models.
-        run: Starts the web server and serves the application.
+        __init__() -> None:
+            Initializes the LightApi, creates database tables, and prepares an empty list of routes.
 
-    Examples:
-        ``` py
-        api = LightApi()
-        api.register({'/person': Person})
-        api.run(host='0.0.0.0', port=8000)
-        ```
+        register(handlers: Dict[str, Type]) -> None:
+            Registers routes for SQLAlchemy models or custom RestEndpoint subclasses.
+
+        run(host: str = '0.0.0.0', port: int = 8000) -> None:
+            Starts the web application and runs the server.
     """
-    
-    def __init__(self):
-        """
-        Initializes the LightApi instance and creates database tables.
 
-        Sets up the web application and attempts to create all database tables
-        using SQLAlchemy. Logs any errors that occur during table creation.
+    def __init__(self, initialize_callback: Callable = None, initialize_arguments: Dict = None) -> None:
         """
+        Initializes the LightApi, sets up the aiohttp application, and creates tables in the database.
+
+        Creates an empty list of routes and attempts to create database tables using SQLAlchemy. Logs the status of
+        table creation.
+
+        Raises:
+            SQLAlchemyError: If there is an error during the creation of tables.
+        """
+        self.initialize(
+            callback=initialize_callback,
+            callback_arguments=initialize_arguments
+        )
         self.app = web.Application()
         self.routes = []
         try:
@@ -79,42 +61,49 @@ class LightApi:
         except SQLAlchemyError as e:
             logging.error(f"Error creating tables: {e}")
 
-    def register(self, models: dict):
+    def initialize(self, callback: Callable = None, callback_arguments: Dict = ()) -> None:
         """
-        Registers API routes for the provided SQLAlchemy models.
+        Initializes the LightApi according to a callable
+        """
+        if not callback:
+            return
+        if not callable(callback):
+            raise TypeError("Callback must be a callable object")
+        logging.debug(f"Initializing LightApi with {callback_arguments}")
+        callback(**callback_arguments)
 
-        For each model provided, this method creates RESTful CRUD handlers
-        and registers them as routes within the application.
+    def register(self, handlers: Dict[str, Type]) -> None:
+        """
+        Registers routes for SQLAlchemy models or custom RestEndpoint classes.
 
         Args:
-            models (dict): A dictionary where the keys are route paths (str)
-            and the values are SQLAlchemy models.
+            handlers (Dict[str, Type]): A dictionary where keys are route paths and values are either:
+                - SQLAlchemy model classes: Routes are created based on the model.
+                - Custom RestEndpoint subclasses: Routes are generated from the RestEndpoint instance.
 
-        Examples:
-            ``` py
-            api = LightApi()
-            api.register({'/person': Person})
-            ```
+        Raises:
+            TypeError: If a handler in the dictionary is neither a SQLAlchemy model nor a RestEndpoint subclass.
         """
-        for path, model in models.items():
-            self.routes.extend(create_handler(model))
+        for path, handler in handlers.items():
+            if issubclass(handler, Base):
+                self.routes.extend(create_handler(handler))
+            elif issubclass(handler, RestEndpoint):
+                endpoint_instance = handler()
+                self.routes.extend(endpoint_instance.routes)
+            else:
+                raise TypeError(
+                    f"Handler for path {path} must be either a SQLAlchemy model or a RestEndpoint subclass."
+                )
+        for r in self.routes:
+            logging.info(f"{r.method} {r.path}")
 
-    def run(self, host='0.0.0.0', port=8000):
+    def run(self, host: str = '0.0.0.0', port: int = 8000) -> None:
         """
-        Starts the web server and serves the application.
-
-        This method adds the registered routes to the application and runs the
-        aiohttp web server on the specified host and port.
+        Starts the web application and begins listening for incoming requests.
 
         Args:
-            host (str, optional): The host IP to bind the server to. Defaults to '0.0.0.0'.
-            port (int, optional): The port number to run the server on. Defaults to 8000.
-
-        Examples:
-            ``` py
-            api = LightApi()
-            api.run(host='127.0.0.1', port=8080)
-            ```
+            host (str): The hostname or IP address to bind the server to. Defaults to '0.0.0.0'.
+            port (int): The port number on which the server will listen. Defaults to 8000.
         """
         self.app.add_routes(self.routes)
         web.run_app(self.app, host=host, port=port)
